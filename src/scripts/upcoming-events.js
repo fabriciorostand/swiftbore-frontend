d8475606edab7785154b247143fcfd3fcabf9698
Fabricio Rostand Morais <fabriciorostand2000@gmail.com>
2025-09-19 20:03:56 -0300

    refactor: upcoming events to dynamic script

---
+++ src/scripts/upcoming-events.js
@@ -0,0 +1,31 @@
+const upcomingEvents = [
+    {
+        name: "Natal",
+        date: "15/12",
+        icon: "../assets/images/upcoming-event-icon.png",
+    },
+    {
+        name: "Ano Novo",
+        date: "27/12",
+        icon: "../assets/images/upcoming-event-icon.png",
+    }
+];
+
+const container = document.getElementById("upcoming-events");
+
+upcomingEvents.forEach(event => {
+    const div = document.createElement("div");
+    div.className = "border border-gray-300 rounded-sm mt-2 p-4 flex items-center gap-4";
+
+    div.innerHTML = `
+        <div class="w-16 h-16 flex items-center justify-center bg-gray-300 rounded-full">
+            <img src="${event.icon}" alt="Ícone do evento">
+        </div>
+        <div class="flex flex-col">
+            <span class="font-medium">${event.name}</span>
+            <p class="text-sm text-gray-600">Começa em ${event.date}</p>
+        </div>
+    `;
+
+    container.appendChild(div);
+});
