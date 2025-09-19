24d08853d163a6a6688f021ab4e27908ad92f9f3
Fabricio Rostand Morais <fabriciorostand2000@gmail.com>
2025-09-19 20:32:17 -0300

    refactor: active event objectives to dynamic script

---
+++ src/scripts/active-event-objectives.js
@@ -0,0 +1,54 @@
+const objectives = [
+    {
+        title: "Vender 150 unidades de Sorvete de Creme Kibon (1,5L)",
+        progress: 0,
+        goal: 150,
+        points: 50
+    },
+    {
+        title: "Vender 300 unidades de Bolinha de Queijo Swift (300g)",
+        progress: 0,
+        goal: 300,
+        points: 200
+    },
+    {
+        title: "Vender 300 unidades de Chicken Croc Tradicional Swift (300g)",
+        progress: 0,
+        goal: 300,
+        points: 200
+    }
+];
+
+// Container 
+const container = document.getElementById("objectives-container");
+
+objectives.forEach(obj => {
+    const percentualProgress = (obj.progress / obj.goal) * 100;
+
+    const div = document.createElement("div");
+    div.className = "border border-gray-300 rounded-sm mt-4 p-4";
+    div.innerHTML = `
+        <div class="flex justify-between items-center mb-8">
+            <span class="font-medium">${obj.title}</span>
+        </div>
+
+        <!-- Progress bar -->
+        <div class="w-full bg-gray-300 rounded-full h-3">
+            <div class="h-3 rounded-full bg-orange-400" style="width: ${percentualProgress}%"></div>
+        </div>
+
+        <!-- Markers -->
+        <div class="flex justify-between text-sm text-gray-500 mt-1">
+            <span>${obj.progress}</span>
+            <span>${obj.goal}</span>
+        </div>
+
+        <!-- Reward button -->
+        <button class="bg-orange-400 text-white p-3 rounded w-full max-w-md mx-auto block mt-8 hover:cursor-pointer hover:bg-orange-500 disabled:opacity-50 disabled:cursor-default" 
+        ${obj.progress < obj.goal ? "disabled" : ""}>
+            Resgatar - ${obj.points} pontos
+        </button>
+    `;
+    
+    container.appendChild(div);
+});
